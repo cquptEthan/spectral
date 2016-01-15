5bc944356b4301763b2b2aecbea4cd23511d6ae0
Ethan <228477911@qq.com>
2016-01-15 18:45:41 +0800

    first commit

---
+++ src/main/java/edu/cqupt/spectral/KMeansMapper.java
@@ -0,0 +1,11 @@
+package edu.cqupt.spectral;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Ethan
+ * Date: 1/15/16
+ * Time: 11:35 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class KMeansMapper {
+}
---
+++ src/main/java/edu/cqupt/spectral/KMeansReducer.java
@@ -0,0 +1,13 @@
+package edu.cqupt.spectral;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Ethan
+ * Date: 1/15/16
+ * Time: 11:36 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class KMeansReducer {
+
+
+}
